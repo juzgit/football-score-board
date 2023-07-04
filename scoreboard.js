c34ed2e2910d2b2cbd91ec3ba8992d3454a3bcab
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2023-07-04 14:34:25 +0200

    update

---
+++ scoreboard.js
@@ -0,0 +1,14 @@
+let minutes = document.getElementsByClassName("minutes");
+let seconds = document.getElementsByClassName("seconds");
+let buttonStart = document.getElementsByClassName("start");
+let buttonStop = document.getElementsByClassName("stop");
+let buttonReset = document.getElementsByClassName("reset");
+var Interval;
+
+function startTimer(){
+  minutes.textContent = "00";
+  seconds.textContent = "00";
+  
+
+
+}
