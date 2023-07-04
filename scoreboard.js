25d6a8f42ec6b0b05d274b31695bb893b8d0887e
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2023-07-04 16:39:17 +0200

    javascript not working

--- scoreboard.js
+++ scoreboard.js
@@ -1,14 +1,38 @@
-let minutes = document.getElementsByClassName("minutes");
-let seconds = document.getElementsByClassName("seconds");
-let buttonStart = document.getElementsByClassName("start");
-let buttonStop = document.getElementsByClassName("stop");
-let buttonReset = document.getElementsByClassName("reset");
-var Interval;
-
-function startTimer(){
-  minutes.textContent = "00";
-  seconds.textContent = "00";
-  
-
-
+window.onload = function(){
+    let minutesEl = document.getElementsByClassName("minutes");
+    let secondsEl = document.getElementsByClassName("seconds");
+    let minutes = 0;
+    let seconds = 0;
+    let buttonStart = document.getElementsByClassName("start");
+    let buttonPause = document.getElementsByClassName("pause");
+    let buttonReset = document.getElementsByClassName("reset");
+    var myInterval;
+    
+   buttonStart = function startTimer(){
+        seconds++;
+    
+        if (seconds <= 9){
+            secondsEl.textContent = "0" + seconds;
+        }
+    
+        if (seconds > 9) {
+            secondsEl.textContent = seconds;
+        }
+    
+        if(seconds > 59){
+            minutes++;
+            minutesEl.textContent = "0" + minutes;
+            seconds = 0;
+            secondsEl.textContent = "0" + seconds;
+        }
+    
+        if (minutes > 9){
+            minutesEl.textContent = minutes;
+        }
+    
+        myInterval = setInterval(startTimer, 1000);
+    }
+    
+    startTimer();  
 }
+
