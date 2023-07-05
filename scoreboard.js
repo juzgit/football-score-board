f3a0bb3fffe4e80cf9b0ac13d199d05fba3f0841
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2023-07-05 16:21:17 +0200

    scrapped js code

--- scoreboard.js
+++ scoreboard.js
@@ -1,37 +1,8 @@
-window.onload = function(){
-    let minutesEl = document.querySelector(".minutes");
-    let secondsEl = document.querySelector(".seconds");
-    let minutes = 0;
-    let seconds = 0;
-    let buttonStart = document.querySelector(".start")
-    let buttonPause = document.querySelector(".pause");
-    let buttonReset = document.querySelector(".reset");
-    var myInterval;
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+const timeHolder = document.getElementsByClassName("time-wrapper");
+
+timeHolder.textContent = function startTimer(){
+    let time = "00:00";
     
-   function startTimer(){
-    seconds++; // seconds increase by 1
-
-    if (seconds <= 9){
-        secondsEl.innerHTML = "0" + seconds; //seconds less 9, display 00:07
-    }
-    
-    if (seconds > 9) {
-        secondsEl.innerHTML= seconds; //seco
-    }
-
-    if(seconds > 59){
-        minutes++;
-        minutesEl.innerHTML = "0" + minutes;
-        seconds = 0;
-        secondsEl.innerHTML = "0" + seconds;
-    }
-
-    if (minutes > 9){
-        minutesEl.innerHTML = minutes;
-    }
-
-    myInterval = setInterval(startTimer, 1000);
-}
- 
- 
 }
