5da516f5571e3d3fc6a88942d8eb3fa7a760aa51
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2023-07-10 15:10:16 +0200

    something is wrong with my logic

--- scoreboard.js
+++ scoreboard.js
@@ -1,8 +1,77 @@
-const minutesEl = document.getElementById("minutes");
-const secondsEl = document.getElementById("seconds");
-const timeHolder = document.getElementsByClassName("time-wrapper");
+let minute = 0;
+let second = 0;
+let interval;
 
-timeHolder.textContent = function startTimer(){
-    let time = "00:00";
-    
+document.timeWrapper.start.onclick =() =>
+start();
+document.timeWrapper.pause.onclick = () =>
+stop();
+document.timeWrapper.reset.onclick = () =>
+reset();
+
+function start(){
+    pause();
+    interval = setInterval(() => { timer(); }, 10);
 }
+
+function pause(){
+    clearInterval(interval);
+}
+
+function reset(){
+    minute = 0;
+    second = 0;
+
+    document.getElementById("minutes").innerText = "00";
+    document.getElementById("seconds").innerText = "00";
+
+}
+
+function timer(){
+    if(second == 60){
+        second = 0;
+        minute++;
+    }
+
+    if(minute == 45 ){
+        pause();
+        alert("It is half-time")
+        minute = 45;
+    }
+
+    if(minute == 90){
+        reset();
+        alert("It is full-time");
+        minute = 90;
+    }
+
+    document.getElementById("minutes").innerText = returnData(minute);
+    document.getElementById("seconds").innerText = returnData(second);
+
+    function returnData(){
+
+    }
+
+}
+
+let team1 = document.getElementsByClassName("score1");
+let team2 = document.getElementsByClassName("score2");
+let team1Goal = document.getElementsByClassName("goal1");
+let team2Goal = document.getElementsByClassName("goal2");
+let scoreDisplay = document.getElementsByClassName("scoreHolder");
+updateScores();
+
+team1Goal.addEventListener("click",()=>{
+    team1++;
+    updateScores();
+
+});
+
+team2Goal.addEventListener("click", ()=>{
+    team2++;
+    updateScores();
+});
+
+function updateScores(){
+    scoreDisplay.innerHTML = team1||team2;
+};
