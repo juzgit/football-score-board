31bbd9f75caf30d9d2059530e54f184904187364
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2023-07-11 15:16:24 +0200

    rough draft code

--- scoreboard.js
+++ scoreboard.js
@@ -8,5 +8,33 @@ function start(){
     minutesElChild.textContent = "00";
     secondsElChild.textContent = "00";
     timeHolder.textContent = minutesElChild + ":" + secondsElChild;
+    let myInterval;
+
+    minutes = minutesElChild;
+    seconds = secondsElChildElChild;
+
+    seconds++;
+    clearInterval(myInterval);
+    myInterval = setInterval(start(), 1000);
     
-}
+    if (seconds <= 9){
+        secondsElChild.textContent= "0" + seconds;
+    
+    } if(seconds > 9){
+        secondsElChild.textContent = seconds;
+   
+    } if(seconds > 59){
+        minutes++;
+        minutesElChild.textContent = "0" + minutes;
+        seconds = 0;
+        secondsElChild.textContent = "0" + seconds
+    
+    } if (minutes > 9){
+        minutesElChild.textContent = minutes;
+    
+    } if(minutes == 45){
+
+    }
+
+    }
+
