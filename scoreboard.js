0460fc5050e3a5eec8a7a2c32e5ad7fb67a45ce5
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2023-07-17 18:11:23 +0200

    it finally works

--- scoreboard.js
+++ scoreboard.js
@@ -1,19 +1,52 @@
-var second = 0;
-var timer = setInterval(start, 1000);
-function start( count ){
-    return count > 9 ? count : '0' + count; }
-    setInterval( function(){
-        document.getElementById('seconds').innerHTML = start(++second % 60);
-        document.getElementById('minutes').innerHTML = start(parseInt(second/ 60, 10));
-    },1000);
+let minutes = 0;
+let seconds = 0;
 
+function start(){
+    timer = true;
+    myCounter();
+}
 
-
+function stop(){
+    timer = false;
+}
 
 function reset(){
-    second = 0;
+    timer = false
+    minutes = 0;
+    seconds = 0;
     document.getElementById('minutes').innerHTML = '00';
     document.getElementById('seconds').innerHTML = '00';
+}
+
+function myCounter(){
+    if(timer){
+        seconds++;
+        
+        if( seconds == 60){
+            minutes++;
+            seconds = 0;
+        }
+
+        if(minutes == 10){
+            alert("The match is finished");
+            reset();
+        }
+
+        let minutesEl = minutes;
+        let secondsEl = seconds;
+
+        if(minutes < 10){
+            minutesEl = "0" + minutes;
+        } 
+
+        if (seconds < 10){
+            secondsEl = "0" + seconds;
+        }
+
+        document.getElementById("minutes").innerHTML = minutesEl;
+        document.getElementById("seconds").innerHTML = secondsEl;
+        setTimeout(myCounter,1000);
+    }
 }
 
 // This works
