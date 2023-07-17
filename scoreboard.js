b7dcb86754d61f84a58653cac53c1e9f2375d9a3
Rameck-Junior Gadziso <rameckjuniorgit@gmail.com>
2023-07-17 17:00:23 +0200

    it works, but cannot stop it.

--- scoreboard.js
+++ scoreboard.js
@@ -1,65 +1,22 @@
-let minutesEl = document.getElementById("minutes");
-let secondsEl = document.getElementById("seconds");
-const startBtn = document.querySelector(".start");
-var myInterval;
-
-startBtn.addEventListener("click", function() {
-
-    minutesEl.innerHTML = "00";
-    secondsEl.innerHTML = "00";
-
-    let fullTime = 11*60;
-
-    let totalTime = parseInt(minutesEl.innerHTML) + parseInt(secondsEl.innerHTML);
-
-    fullTime = totalTime;
-    
-
-    secondsEl++;
-   
-    if(secondsEl.innerHTML <= 9){
-        totalTime = minutesEl.innerHTML + secondsEl.innerHTML;
-    } if(secondsEl.innerHTML == 9){
-        totalTime = minutesEl.innerHTML + parseInt("10") + secondsEl.innerHTML;
-    } if(secondsEl.innerHTML <= 19 ){
-        totalTime = minutesEl.innerHTML + secondsEl.innerHTML;
-    } if(secondsEl.innerHTML == 19){
-        totalTime = minutesEl.innerHTML + parseInt("20") + secondsEl.innerHTML;
-    } if(secondsEl.innerHTML <= 29){
-        totalTime = minutesEl.innerHTML + secondsEl.innerHTML;
-    } if(secondsEl.innerHTML == 29){
-        totalTime = minutesEl.innerHTML + parseInt("30") + secondsEl.innerHTML;
-    } if (secondsEl.innerHTML <= 39){
-        totalTime = minutesEl.innerHTML + secondsEl.innerHTML;
-    } if (secondsEl.innerHTML == 39){
-        totalTime = minutesEl.innerHTML + parseInt("40") + secondsEl.innerHTML;
-    } if (secondsEl.innerHTML <= 49 ){
-        totalTime = minutesEl.innerHTML + secondsEl.innerHTML;
-    } if (secondsEl.innerHTML == 49){
-        totalTime = minutesEl.innerHTML + parseInt("50") + secondsEl.innerHTML;
-    } if(secondsEl.innerHTML <= 59){
-        totalTime = minutesEl.innerHTML + secondsEl.innerHTML;
-    } if(secondsEl.innerHTML == 59){
-        minutesEl.innerHTML++;
-        totalTime = minutesEl.innerHTML + secondsEl.innerHTML;
-    }
-}); 
+var second = 0;
+function start( count ){
+    return count > 9 ? count : '0' + count; }
+    setInterval( function(){
+        document.getElementById('seconds').innerHTML = start(++second % 60);
+        document.getElementById('minutes').innerHTML = start(parseInt(second/ 60, 10));
+    },1000);
 
 
 
-function stop(){
-    clearInterval(myInterval);
-    setTimeout(myInterval,1000);
-}
 
-function reset(){
+/* function reset(){
     clearInterval(myInterval);
     seconds = "00";
     minutes = "00";
     secondsEl.textContent = seconds;
     minutesEl.textContent = minutes;
    
-}
+} */
 
 // This works
 const teamOne = document.getElementById("score1");
